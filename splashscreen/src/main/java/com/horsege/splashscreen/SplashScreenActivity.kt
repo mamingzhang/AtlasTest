c4f8e66a920f0fefdd2c3eaa74bece29a1109e06
mamingzhang <mamingzhang@shuzijiayuan.com>
2017-08-21 17:39:34 +0800

    增加启动页

--- splashscreen/src/main/java/com/horsege/splashscreen/SplashScreenActivity.kt
+++ splashscreen/src/main/java/com/horsege/splashscreen/SplashScreenActivity.kt
@@ -1,12 +1,33 @@
 package com.horsege.splashscreen
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.PersistableBundle
 import com.horsege.middleawaylibrary.view.activity.BaseActivity
+import org.jetbrains.anko.contentView
+import org.jetbrains.anko.startActivity
 
 class SplashScreenActivity : BaseActivity<SplashScreenLayout>() {
     override val ui = SplashScreenLayout()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+
+        contentView?.postDelayed({
+            try {
+                val intent = Intent()
+                intent.setClassName(baseContext, "com.horsege.atlastest.view.activity.MainActivity")
+                startActivity(intent)
+            } catch (e : Exception) {
+                e.printStackTrace()
+            }
+
+
+            finish()
+        },3 * 1000 )
     }
 }
